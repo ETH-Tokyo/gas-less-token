ed49e51bcbd54c6724acdc2baa0b366c1e397205
eri2490 <e.147xxx@gmail.com>
2023-04-14 19:58:53 +0900

    chore: comment out prettier check

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,13 +8,13 @@ const buildSolhintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" ")} `;
 
-const buildPrettierCommand = (filenames) =>
-  `prettier --check ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")} `;
+// const buildPrettierCommand = (filenames) =>
+//   `prettier --check ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(" ")} `;
 
 module.exports = {
   "**/*.{js,jsx,ts,tsx}": [buildLintCommand],
-  "**/*.{js,jsx,ts,tsx,sol}": [buildPrettierCommand],
+  // "**/*.{js,jsx,ts,tsx,sol}": [buildPrettierCommand],
   "**/*.sol": [buildSolhintCommand],
 };
