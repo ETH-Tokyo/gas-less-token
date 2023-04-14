08c748ef9283b460849b6d83c77ea20f5ca8027c
eri2490 <e.147xxx@gmail.com>
2023-04-15 01:19:47 +0900

    feat: add wallet connect config

--- packages/front/pages/_app.tsx
+++ packages/front/pages/_app.tsx
@@ -1,12 +1,40 @@
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
 import { AppProps } from "next/app";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 import "/styles/globals.css";
 
+const { chains, provider } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY || "" }),
+    publicProvider(),
+  ],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "PAY FOR GAS",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
