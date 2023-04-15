27cbfcae91dcb1630c29826f69ced4d9b87f4014
eri2490 <e.147xxx@gmail.com>
2023-04-16 01:06:44 +0900

    feat: add header

---
+++ packages/front/components/template/header.tsx
@@ -0,0 +1,21 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center mb-16">
+      <div>
+        <Link href="/owner">
+          <Image
+            src="/logo/gasslesstoken.png"
+            alt="gasslesstoken"
+            width={150}
+            height={100}
+          />
+        </Link>
+      </div>
+      <ConnectButton />
+    </div>
+  );
+};
