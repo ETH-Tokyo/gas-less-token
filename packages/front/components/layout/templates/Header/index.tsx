37ca1e0aa5d53d2c4543a96744a68e9e86bc78a4
SHØ <neila@users.noreply.github.com>
2023-04-15 14:45:42 +0900

    apply prettier

--- packages/front/components/layout/templates/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
-export const Header = () => {
-  return (
-    <div style={{ display: "flex", justifyContent: "flex-end" }}>
-      <ConnectButton />
-    </div>
-  );
-};
--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,59 +1,6 @@
-import { Container, Tab, Tabs } from "@mui/material";
 import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-
-import CreateAccount from "./create-account";
-import CreateTokenPaymaster from "./create-tokenpaymaster";
 
 import Layout from "@/components/layout/baseLayout";
-
-// import SendTx from "./send-tx";
-
-// function TabPanel(props) {
-//   const { children, value, index, ...other } = props;
-
-//   return (
-//     <div
-//       role="tabpanel"
-//       hidden={value !== index}
-//       id={`tabpanel-${index}`}
-//       aria-labelledby={`tab-${index}`}
-//       {...other}
-//     >
-//       {value === index && <div>{children}</div>}
-//     </div>
-//   );
-// }
-
-// function MyTabs() {
-//   const [value, setValue] = useState(0);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
-
-//   return (
-//     <Container maxWidth="md">
-//       <Tabs value={value} onChange={handleChange}>
-//         <Tab label="create token paymaster" id="tab-0" />
-//         <Tab label="create account" id="tab-1" />
-//         <Tab label="send tx" id="tab-2" />
-//       </Tabs>
-//       <TabPanel value={value} index={0}>
-//         <CreateTokenPaymaster />
-//       </TabPanel>
-//       <TabPanel value={value} index={1}>
-//         <CreateAccount />
-//       {/*
-//         </TabPanel>
-//         <TabPanel value={value} index={2}>
-//         <SendTx />
-//       // */}
-//       </TabPanel>
-//     </Container>
-//   );
-// }
 
 export default function Home() {
   return (
